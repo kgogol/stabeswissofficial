95bfab620243408cb8f9775c7819af5d8af598d8
kgogol <krzys.gogol@gmail.com>
2022-01-24 17:25:31 +0100

    Update SwissSC.sol

--- Solidity/SwissSC.sol
+++ Solidity/SwissSC.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract SwissSC is ERC20 
 {
         
-    constructor() ERC20("SWISS Stable coin", "SwissSC") {
+    constructor() ERC20(" Stable Swiss Coin", "CHFsn") {
         
     } 
 
