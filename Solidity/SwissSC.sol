6f4dfa118bb98e729fcd3c006ffee227d24d61e3
kgogol <krzys.gogol@gmail.com>
2022-01-24 17:20:00 +0100

    Update SwissSC.sol

--- Solidity/SwissSC.sol
+++ Solidity/SwissSC.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/tree/release-v4.5/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SwissSC is ERC20 
 {
