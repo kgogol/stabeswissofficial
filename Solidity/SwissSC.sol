65b7126b67201da67f9bd4a6321e34e9171deb53
kgogol <krzys.gogol@gmail.com>
2022-04-07 17:52:57 +0200

    Update SwissSC.sol

--- Solidity/SwissSC.sol
+++ Solidity/SwissSC.sol
@@ -9,4 +9,17 @@ contract SwissSC is ERC20
         
     } 
 
+    function mint(address to, uint256 amount)
+        external
+        returns (bool)
+    {
+        _mint(to, amount);
+        return true;
+    } 
+    
+    function burn(uint256 amount) external returns (bool) {
+        _burn(_msgSender(), amount);
+        return true;
+    }
+
 }    
